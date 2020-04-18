80f87e2ca62981aff3f9396a105d56f275068108
ahmedprog <ahmed.6nasser@gmail.com>
2020-04-18 22:16:29 +0200

    rebuilding site 18 أبر, 2020 EET 10:16:29 م

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,21 @@
 # If a command fails then the deploy stops
 set -e
 
+printf "\033[0;32mPush updates to GitHub AN_Portfolio repo...\033[0m\n"
+
+
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
---
+++ themes/hugo-resume/content/projects/backend/_index.md
@@ -0,0 +1,7 @@
+---
+title: "Backend"
+sitemap:
+  priority : 0.9
+weight: 10
+---
+<p>A collection of projects authored by Ahmed Nasser, and likely shared out with the community as an open source project.</p>
